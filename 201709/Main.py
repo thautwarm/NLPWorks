87d7369d81ff6b6d3c93e04aa840eb4dc81f6501
thautwarm <twshere@outlook.com>
2017-10-06 12:57:11 +0800

    update-to-all

--- 201709/Main.py
+++ 201709/Main.py
@@ -1,7 +1,7 @@
 from validate import validateOn, model
 from algorithm.Methods import  methods
 from algorithm.decomposition import nameAssemble
-for method_name in methods:
+for gram in ('gram-1', 'gram-2','gram-3','gram-mix'):
     for decomp in nameAssemble:
-        for gram in ('gram-1', 'gram-2','gram-3','gram-mix'):
+        for method_name in methods:
             validateOn(method_name, decomp, gram)
