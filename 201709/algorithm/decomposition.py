3c7f02509bfdd04fecdbdc1f12b04694eb65614b
thautwarm <twshere@outlook.com>
2017-10-06 12:30:19 +0800

    load

--- 201709/algorithm/decomposition.py
+++ 201709/algorithm/decomposition.py
@@ -12,15 +12,17 @@ from sklearn.lda import LDA
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest
-from scipy.stats import pearsonr
+# from scipy.stats import pearsonr
 from sklearn.feature_selection import chi2,f_classif
-from minepy import MINE
-def MIC(x, y):
-     # Maximal Information Coefficient 
-     base = MINE()
-     base.compute_score(x, y)
-     return base.mic(),0.5  # just one kind of factor,
-                            # but SelectKBest needs two factors to fit datas.
+# from minepy import MINE
+# def MIC(X, y):
+#      # Maximal Information Coefficient
+#      base = MINE()
+#      return np.array( [ (base.compute_score(x, y) or base.mic(), 0.5) for x in X.T ]).T
+#      # just one kind of factor,
+#      # but SelectKBest needs two factors to fit datas.
+# def Pearsonr(X,y):
+#     return np.array([pearsonr(x,y) for x in X.T]).T
 
 def de_pca(X,y):
     dim = X.shape[1]
@@ -43,15 +45,15 @@ def de_lda(X,y):
         return clf.transform(X1), clf.transform(X2)
     return _func
 
-def de_ps(X,y):
-    """ pearsonr method """
-    dim = X.shape[1]
-    de = min(2000,dim)
-    clf = SelectKBest(lambda X, Y: np.array(map(lambda x:pearsonr(x, Y), X.T)).T, k=de)
-    clf.fit(X,y)
-    def _func(X1,X2):
-        return clf.transform(X1),clf.transform(X2)
-    return _func
+# def de_ps(X,y):
+#     """ pearsonr method """
+#     dim = X.shape[1]
+#     de = min(2000,dim)
+#     clf = SelectKBest(Pearsonr , k=de)
+#     clf.fit(X,y)
+#     def _func(X1,X2):
+#         return clf.transform(X1),clf.transform(X2)
+#     return _func
 
 def de_rf(X,y):
     """random forest"""
@@ -60,8 +62,8 @@ def de_rf(X,y):
     clf  = RandomForestClassifier(n_estimators=200)
     _,x_mini,_,y_mini = train_test_split(X,y,test_size = 0.33)
     clf.fit(x_mini,y_mini)
+    index = np.argsort(clf.feature_importances_)[::-1][:de].astype(np.int32)
     def _func(X1,X2):
-        index = np.sort(clf.feature_importances_)[::-1][:de]
         return X1[:,index],X2[:,index]
     return _func
 
@@ -72,18 +74,18 @@ def de_c2(X,y):
     clf  = SelectKBest(chi2, k = de)
     clf.fit(X,y)
     def _func(X1,X2):
-        return clf.transform(X1),clf.transform(X2)
+        return clf.transform(X1), clf.transform(X2)
     return _func
 
-def de_mic(X,y):
-    """ MIC """
-    dim  = X.shape[1]
-    de   = min(2000,dim) 
-    clf = SelectKBest(MIC, k=de)
-    clf.fit(X,y)
-    def _func(X1,X2):
-        return clf.transform(X1),clf.transform(X2)
-    return _func
+# def de_mic(X,y):
+#     """ MIC """
+#     dim  = X.shape[1]
+#     de   = min(2000,dim) 
+#     clf = SelectKBest(MIC, k=de)
+#     clf.fit(X,y)
+#     def _func(X1,X2):
+#         return clf.transform(X1),clf.transform(X2)
+#     return _func
 
 def de_f_and_p_value(X,y):
     """ f&p value """
